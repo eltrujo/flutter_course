f4a80b7fbee246877f7b4d9c517b79be9412671c
EL TRUJO <eltrujo1@gmail.com>
2019-09-23 20:20:32 +0200

    Added magic_8_ball app

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,19 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-  var leftDiceNumber = 2;
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,17 +38,21 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button pressed.');
+                setState(() {
+                  changeDiceFace();
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right button pressed.');
+                setState(() {
+                  changeDiceFace();
+                });
               },
-              child: Image.asset('images/dice4.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           )
         ],
